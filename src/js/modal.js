42de0eb08213ecee5103b8e885b22d53c82c8d0b
velmoren <velmoren@yahoo.com>
2019-03-05 18:19:11 +0300

    добавлена секция price, реализован адаптив

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
   var button = $('#button');
+  var buttonTwo = $('#button__2');
   var modal = $('#modal');
   var close = $('#close');
+  var autoClose;
   button.on('click', function() {
     modal.addClass('modal_active');
+    document.body.style.overflow = 'hidden';
+    autoClose = setTimeout(closeModal, 5000);
   });
-  close.on('click', function() {
+
+  buttonTwo.on('click', function() {
+    modal.addClass('modal_active');
+    document.body.style.overflow = 'hidden';
+    autoClose = setTimeout(closeModal, 5000);
+  });
+
+  close.on('click', closeModal);
+  function closeModal() {
     modal.removeClass('modal_active');
-  });
+    document.body.style.overflow = '';
+    clearTimeout(autoClose);
+  }
 });
+
