d2446d146a8cc6c09445a0a92f2e4c197728a89c
velmoren <velmoren@yahoo.com>
2019-03-06 11:23:42 +0300

    решена проблема с модальным окном, реализовано на JQuery

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,26 +1,43 @@
 $(document).ready(function() {
   var button = $('#button');
   var buttonTwo = $('#button__2');
-  var modal = $('#modal');
+  var modal = $('#modal');/* оставил на будущюю реализацию */
   var close = $('#close');
-  var autoClose;
+  var wrap = $('#wrapper');
+
+  var timer; /* переменная, принимающая таймер */
+
   button.on('click', function() {
-    modal.addClass('modal_active');
-    document.body.style.overflow = 'hidden';
-    autoClose = setTimeout(closeModal, 5000);
+    wrap.fadeIn();
+    OffScroll ();
+    timer = setTimeout(function() { 
+      wrap.fadeOut();
+      $(window).unbind('scroll');
+    }, 5000);
   });
 
   buttonTwo.on('click', function() {
-    modal.addClass('modal_active');
-    document.body.style.overflow = 'hidden';
-    autoClose = setTimeout(closeModal, 5000);
+    wrap.fadeIn();
+    OffScroll (); /* вызываем функцию остановки прокрутки */
+    /* задаем таймер закрытия окна через 5 сек */
+    timer = setTimeout(function() { 
+      wrap.fadeOut();
+      $(window).unbind('scroll');
+    }, 5000);
   });
 
-  close.on('click', closeModal);
-  function closeModal() {
-    modal.removeClass('modal_active');
-    document.body.style.overflow = '';
-    clearTimeout(autoClose);
-  }
+  close.on('click', function() {
+    wrap.fadeOut();
+    clearTimeout(timer); /* отчищаем переменную таймер */
+    $(window).unbind('scroll'); /* снова позволяем прокрутку сайта */
+  });
 });
+
+/* функция остановки прокрутки во время появления модального окна без убирания самой полосы прокрутки */
+function OffScroll () {
+  var winScrollTop = $(window).scrollTop();
+  $(window).bind('scroll',function () {
+    $(window).scrollTop(winScrollTop);
+});}
+
 
