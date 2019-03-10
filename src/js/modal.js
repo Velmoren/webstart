74b784e289167fbed19d055e94afa25cf176122c
velmoren <velmoren@yahoo.com>
2019-03-10 14:37:34 +0300

    коммит работы с другого ПК за 2 дня

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,32 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var button = $('#button');
-  var buttonTwo = $('#button__2');
-  var modal = $('#modal');/* оставил на будущюю реализацию */
+  var modal = $('#modal'); /* оставил на будущюю реализацию */
   var close = $('#close');
   var wrap = $('#wrapper');
 
   var timer; /* переменная, принимающая таймер */
 
-  button.on('click', function() {
+  button.on('click', function () {
     wrap.fadeIn();
-    OffScroll ();
-    timer = setTimeout(function() { 
+    OffScroll();
+    timer = setTimeout(function () {
       wrap.fadeOut();
       $(window).unbind('scroll');
     }, 5000);
   });
 
-  buttonTwo.on('click', function() {
-    wrap.fadeIn();
-    OffScroll (); /* вызываем функцию остановки прокрутки */
-    /* задаем таймер закрытия окна через 5 сек */
-    timer = setTimeout(function() { 
-      wrap.fadeOut();
-      $(window).unbind('scroll');
-    }, 5000);
-  });
-
-  close.on('click', function() {
+  close.on('click', function () {
     wrap.fadeOut();
     clearTimeout(timer); /* отчищаем переменную таймер */
     $(window).unbind('scroll'); /* снова позволяем прокрутку сайта */
@@ -34,10 +23,9 @@ $(document).ready(function() {
 });
 
 /* функция остановки прокрутки во время появления модального окна без убирания самой полосы прокрутки */
-function OffScroll () {
+function OffScroll() {
   var winScrollTop = $(window).scrollTop();
-  $(window).bind('scroll',function () {
+  $(window).bind('scroll', function () {
     $(window).scrollTop(winScrollTop);
-});}
-
-
+  });
+}
